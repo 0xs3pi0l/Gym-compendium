73c1d8c5b3511ab5fb481cfca8e7eb569f1448ce
0xs3pi0l <omarpasculli2001@gmail.com>
2024-01-03 12:01:40 +0100

    Add general script structure

---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+let button = document.getElementsByTagName("button")[0];
+button.addEventListener("click", fetchContent);
+
+function fetchContent(){
+    //getParameters();
+    //Fetch quote ...
+    //swapContent(content);
+}
+
+function swapContent(){
+    let cardsContainer = document.getElementsByClassName("cards-container");
+    //Swap content ...
+}
