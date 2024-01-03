2279ded931121b5532b69c91742478802cddf574
0xs3pi0l <omarpasculli2001@gmail.com>
2024-01-03 16:09:21 +0100

    Add crossOrigin annotation to solve CORS problem

--- backend/src/main/java/GymAPI/project/web/ExerciseController.java
+++ backend/src/main/java/GymAPI/project/web/ExerciseController.java
@@ -2,15 +2,13 @@ package GymAPI.project.web;
 
 import GymAPI.project.Exercise;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import GymAPI.project.data.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5500")
 @RestController
 @RequestMapping
 public class ExerciseController {
