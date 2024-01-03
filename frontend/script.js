86a9b2fdb696fde27c88a9244c55841dfece12b0
0xs3pi0l <omarpasculli2001@gmail.com>
2024-01-03 16:34:20 +0100

    Implement function to fetch data from the API

--- frontend/script.js
+++ frontend/script.js
@@ -2,12 +2,21 @@ let button = document.getElementsByTagName("button")[0];
 button.addEventListener("click", fetchContent);
 
 function fetchContent(){
-    //getParameters();
-    //Fetch quote ...
+    
+    console.log("OK");
+    let selects = document.getElementsByTagName("select");
+    let muscle = selects[0].value;
+    let equipment = selects[1].value;
+    const url = "http://localhost:8080/exercise?muscle=" + muscle + "&equipment=" + equipment;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => swapContent(data))
     //swapContent(content);
 }
 
-function swapContent(){
+
+function swapContent(jsonData){
     let cardsContainer = document.getElementsByClassName("cards-container");
-    //Swap content ...
+    console.log(jsonData);
 }
