c7226149417a84930bbd0b01f89aff370c17ab18
0xs3pi0l <omarpasculli2001@gmail.com>
2024-01-03 17:28:02 +0100

    Implement function to create new exercise card element

--- frontend/script.js
+++ frontend/script.js
@@ -17,6 +17,26 @@ function fetchContent(){
 
 
 function swapContent(jsonData){
-    let cardsContainer = document.getElementsByClassName("cards-container");
-    console.log(jsonData);
+    let cardsContainer = document.getElementsByClassName("cards-container")[0];
+    let exerciseCard = createCard(jsonData[0]);
+    cardsContainer.appendChild(exerciseCard);
+    
+}
+
+function createCard(jsonData){
+    let muscle = jsonData.muscle;
+    let equipment = jsonData.equipment;
+    let embedLink = jsonData.link.substring(32,43)
+    console.log(embedLink);
+    let exerciseCard = document.createElement("div");
+    exerciseCard.className="exercise-card"
+    exerciseCard.innerHTML = `
+    <h3>Exercise name</h3>
+    <p><b>Muscle</b> : ${muscle}</p>
+    <p><b>Equipment</b> : ${equipment}</p>
+    <iframe width="500em" height="350em" src="https://www.youtube.com/embed/${embedLink}"" 
+    frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+    encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    `;
+    return exerciseCard;
 }
