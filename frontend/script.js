ff5410c1fe832808ac0d22d42aaecbbf85659752
0xs3pi0l <omarpasculli2001@gmail.com>
2024-01-04 11:48:19 +0100

    Implement function to insert fetched content into page

--- frontend/script.js
+++ frontend/script.js
@@ -16,19 +16,27 @@ function fetchContent(){
 function insertContent(jsonData){
     let cardsContainer = document.getElementsByClassName("cards-container")[0];
     cardsContainer.innerHTML = '';
-    for(let i = 0; i < jsonData.length; i++){
-        cardsContainer.appendChild(createCard(jsonData[i]));
-    }    
+    if(!jsonData.length){
+        let notFound = document.createElement("div");
+        notFound.className = "notfound-div"
+        notFound.innerHTML = "<p>No content found</p>"
+        cardsContainer.appendChild(notFound);
+    } else {  
+        for(let i = 0; i < jsonData.length; i++){
+            cardsContainer.appendChild(createCard(jsonData[i]));
+        }   
+    }
 }
 
 function createCard(jsonData){
+    let name = jsonData.name;
     let muscle = jsonData.muscle;
     let equipment = jsonData.equipment;
     let embedLink = jsonData.link.substring(32,43)
     let exerciseCard = document.createElement("div");
     exerciseCard.className="exercise-card"
     exerciseCard.innerHTML = `
-    <h3>Exercise name</h3>
+    <h3>${name}</h3>
     <p><b>Muscle</b> : ${muscle}</p>
     <p><b>Equipment</b> : ${equipment}</p>
     <iframe width="500em" height="350em" src="https://www.youtube.com/embed/${embedLink}"" 
